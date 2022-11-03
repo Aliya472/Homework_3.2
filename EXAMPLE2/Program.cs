32434457fa575bbc11e0e9dd135eb8116bf88be8
alya <alya-892@yandex.ru>
2022-11-03 16:46:57 +0300

    Задача 2

---
+++ EXAMPLE2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите числo"); 
+int a=  Convert.ToInt32 (Console.ReadLine()); 
+if( a>99) 
+{ 
+Console.WriteLine(a.ToString()[2]); 
+} 
+else 
+Console.WriteLine( " Такого числа нет ");
